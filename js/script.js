56cdbcbc28f025b54e4ce8d0ec22c6e6285b72a2
Erick-Matheus25 <erickmatheusdasilvavieira2@email.com>
2026-05-22 00:21:37 -0300

    commit 3.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+/* =========================
+   VARIAVEIS
+========================= */
+
 let vidas = 3;
+
+let luzLigada = false;
+
+/* =========================
+   COFRE
+========================= */
+
+let totalMoedas = 0;
+
+let moedas10 = 0;
+let moedas25 = 0;
+let moedas50 = 0;
+let moedas1 = 0;
 
 /* =========================
    IMAGENS
@@ -31,7 +48,7 @@ function abrirJogo(jogo){
 
     area.innerHTML = `
 
-      <h2>Jokenpô</h2>
+      <h2>🎮 Jokenpô</h2>
 
       <div id="vidasBox">
 
@@ -72,7 +89,7 @@ function abrirJogo(jogo){
 
     area.innerHTML = `
 
-      <h2>Batalha de Dados</h2>
+      <h2>🎲 Batalha de Dados</h2>
 
       <div id="vidasBox">
 
@@ -107,7 +124,7 @@ function abrirJogo(jogo){
 
     area.innerHTML = `
 
-      <h2>Cara ou Coroa</h2>
+      <h2>🪙 Cara ou Coroa</h2>
 
       <div id="vidasBox">
 
@@ -142,10 +159,188 @@ function abrirJogo(jogo){
 
   }
 
+  /* =========================
+     INTERRUPTOR
+  ========================= */
+
+  else if(jogo === "interruptor"){
+
+    area.innerHTML = `
+
+      <h2>💡 Controle de Luz</h2>
+
+      <div class="cenario-luz">
+
+        <img
+          id="cenarioBg"
+          class="cenario-bg"
+          src="./quartoescuro.png">
+
+        <div class="tomada-box">
+
+          <img
+            id="tomada"
+            class="tomada-img"
+            src="./tomadadesligada.png"
+            onclick="alternarLuz()">
+
+        </div>
+
+      </div>
+
+    `;
+
+  }
+
+  /* =========================
+     COFRE DIGITAL
+  ========================= */
+
+  else if(jogo === "cofre"){
+
+    carregarCofre();
+
+    area.innerHTML = `
+
+      <h2>🏦 Cofrinho Digital</h2>
+
+      <div class="cofre-box">
+
+        <img
+          class="caldeirao"
+          src="./caldeirão.png">
+
+        <div id="coins"></div>
+
+      </div>
+
+      <div class="total-box">
+
+        <h3>
+
+          Saldo Total
+
+        </h3>
+
+        <div class="total">
+
+          R$
+          <span id="valorTotal">
+
+            ${formatarValor(totalMoedas)}
+
+          </span>
+
+        </div>
+
+      </div>
+
+      <div class="contador-moedas">
+
+        <div class="contador-card">
+
+          🪙 10¢:
+          <span id="m10">
+
+            ${moedas10}
+
+          </span>
+
+        </div>
+
+        <div class="contador-card">
+
+          🪙 25¢:
+          <span id="m25">
+
+            ${moedas25}
+
+          </span>
+
+        </div>
+
+        <div class="contador-card">
+
+          🪙 50¢:
+          <span id="m50">
+
+            ${moedas50}
+
+          </span>
+
+        </div>
+
+        <div class="contador-card">
+
+          💰 R$1:
+          <span id="m1">
+
+            ${moedas1}
+
+          </span>
+
+        </div>
+
+      </div>
+
+      <div class="moedas">
+
+        <button
+          onclick="adicionarMoeda(0.10)">
+
+          + R$0,10
+
+        </button>
+
+        <button
+          onclick="adicionarMoeda(0.25)">
+
+          + R$0,25
+
+        </button>
+
+        <button
+          onclick="adicionarMoeda(0.50)">
+
+          + R$0,50
+
+        </button>
+
+        <button
+          onclick="adicionarMoeda(1)">
+
+          + R$1,00
+
+        </button>
+
+      </div>
+
+      <div class="acoes-cofre">
+
+        <button
+          onclick="sacarValor()">
+
+          💸 Sacar
+
+        </button>
+
+        <button
+          onclick="esvaziarCofre()">
+
+          🗑️ Esvaziar
+
+        </button>
+
+      </div>
+
+    `;
+
+  }
+
 }
 
 /* =========================
-   MOSTRAR VIDAS
+   VIDAS
 ========================= */
 
 function mostrarVidas(){
@@ -167,10 +362,6 @@ function mostrarVidas(){
   `;
 
 }
-
-/* =========================
-   ATUALIZAR VIDAS
-========================= */
 
 function atualizarVidas(){
 
@@ -197,31 +388,23 @@ function atualizarVidas(){
 
 }
 
-/* =========================
-   PERDER VIDA
-========================= */
-
 function perderVida(){
 
   vidas--;
 
   atualizarVidas();
 
-  document.body.classList.add(
-    "tela-tremendo"
-  );
-
-  setTimeout(()=>{
-
-    document.body.classList.remove(
-      "tela-tremendo"
-    );
-
-  },300);
-
   if(vidas <= 0){
 
     mostrarGameOver();
+
+    vidas = 3;
+
+    setTimeout(()=>{
+
+      atualizarVidas();
+
+    },100);
 
   }
 
@@ -234,39 +417,40 @@ function perderVida(){
 function jogarJokenpo(escolha){
 
   const opcoes = [
-
     "Pedra",
     "Papel",
     "Tesoura"
-
   ];
 
   const pc =
     opcoes[
-      Math.floor(
-        Math.random()*3
-      )
+      Math.floor(Math.random()*3)
     ];
 
   let resultado = "";
 
   if(escolha === pc){
 
-    resultado = "⚖️ EMPATE";
+    resultado =
+      "⚖️ EMPATE";
 
   }
 
   else if(
 
-    (escolha === "Pedra" && pc === "Tesoura") ||
+    (escolha === "Pedra" &&
+    pc === "Tesoura") ||
 
-    (escolha === "Papel" && pc === "Pedra") ||
+    (escolha === "Papel" &&
+    pc === "Pedra") ||
 
-    (escolha === "Tesoura" && pc === "Papel")
+    (escolha === "Tesoura" &&
+    pc === "Papel")
 
   ){
 
-    resultado = "🎉 VOCÊ GANHOU";
+    resultado =
+      "🎉 VOCÊ GANHOU";
 
     mostrarVictory();
 
@@ -274,7 +458,8 @@ function jogarJokenpo(escolha){
 
   else{
 
-    resultado = "❌ VOCÊ PERDEU";
+    resultado =
+      "❌ VOCÊ PERDEU";
 
     perderVida();
 
@@ -284,17 +469,17 @@ function jogarJokenpo(escolha){
     "resultado"
   ).innerHTML = `
 
+    ${resultado}
+
+    <br><br>
+
     Você:
     ${escolha}
 
-    <br><br>
+    <br>
 
     PC:
     ${pc}
-
-    <br><br>
-
-    ${resultado}
 
   `;
 
@@ -316,9 +501,6 @@ function jogarDado(){
   );
 
   setTimeout(()=>{
-
-    img.src =
-      "./dadojumper.png";
 
     img.classList.remove(
       "girando"
@@ -440,6 +622,289 @@ function jogarMoeda(escolha){
     }
 
   },1000);
+
+}
+
+/* =========================
+   INTERRUPTOR
+========================= */
+
+function alternarLuz(){
+
+  const tomada =
+    document.getElementById(
+      "tomada"
+    );
+
+  const cenario =
+    document.getElementById(
+      "cenarioBg"
+    );
+
+  luzLigada = !luzLigada;
+
+  tomada.src = luzLigada
+    ? "./tomadaligada.png"
+    : "./tomadadesligada.png";
+
+  cenario.src = luzLigada
+    ? "./quarto-claro.png"
+    : "./quartoescuro.png";
+
+}
+
+/* =========================
+   COFRE
+========================= */
+
+function adicionarMoeda(valor){
+
+  totalMoedas += valor;
+
+  if(valor === 0.10){
+
+    moedas10++;
+
+  }
+
+  else if(valor === 0.25){
+
+    moedas25++;
+
+  }
+
+  else if(valor === 0.50){
+
+    moedas50++;
+
+  }
+
+  else if(valor === 1){
+
+    moedas1++;
+
+  }
+
+  atualizarTela();
+
+  salvarCofre();
+
+  criarMoedaAnimada(valor);
+
+}
+
+function atualizarTela(){
+
+  document.getElementById(
+    "valorTotal"
+  ).textContent =
+    formatarValor(totalMoedas);
+
+  document.getElementById(
+    "m10"
+  ).textContent =
+    moedas10;
+
+  document.getElementById(
+    "m25"
+  ).textContent =
+    moedas25;
+
+  document.getElementById(
+    "m50"
+  ).textContent =
+    moedas50;
+
+  document.getElementById(
+    "m1"
+  ).textContent =
+    moedas1;
+
+}
+
+function formatarValor(valor){
+
+  return valor.toLocaleString(
+    "pt-BR",
+    {
+      minimumFractionDigits:2,
+      maximumFractionDigits:2
+    }
+  );
+
+}
+
+function sacarValor(){
+
+  const valor =
+    parseFloat(
+      prompt(
+        "Digite o valor do saque:"
+      )
+    );
+
+  if(isNaN(valor)){
+
+    return;
+
+  }
+
+  if(valor > totalMoedas){
+
+    alert(
+      "Você não tem Saldo para o saque!!"
+    );
+
+    return;
+
+  }
+
+  totalMoedas -= valor;
+
+  atualizarTela();
+
+  salvarCofre();
+
+  alert(
+    "💸 Saque realizado!"
+  );
+
+}
+
+function esvaziarCofre(){
+
+  totalMoedas = 0;
+
+  moedas10 = 0;
+  moedas25 = 0;
+  moedas50 = 0;
+  moedas1 = 0;
+
+  atualizarTela();
+
+  salvarCofre();
+
+  alert(
+    "🗑️ Cofre esvaziado!"
+  );
+
+}
+
+function salvarCofre(){
+
+  localStorage.setItem(
+    "totalMoedas",
+    totalMoedas
+  );
+
+  localStorage.setItem(
+    "moedas10",
+    moedas10
+  );
+
+  localStorage.setItem(
+    "moedas25",
+    moedas25
+  );
+
+  localStorage.setItem(
+    "moedas50",
+    moedas50
+  );
+
+  localStorage.setItem(
+    "moedas1",
+    moedas1
+  );
+
+}
+
+function carregarCofre(){
+
+  totalMoedas =
+    parseFloat(
+      localStorage.getItem(
+        "totalMoedas"
+      )
+    ) || 0;
+
+  moedas10 =
+    parseInt(
+      localStorage.getItem(
+        "moedas10"
+      )
+    ) || 0;
+
+  moedas25 =
+    parseInt(
+      localStorage.getItem(
+        "moedas25"
+      )
+    ) || 0;
+
+  moedas50 =
+    parseInt(
+      localStorage.getItem(
+        "moedas50"
+      )
+    ) || 0;
+
+  moedas1 =
+    parseInt(
+      localStorage.getItem(
+        "moedas1"
+      )
+    ) || 0;
+
+}
+
+function criarMoedaAnimada(valor){
+
+  const coin =
+    document.createElement("img");
+
+  if(valor === 0.10){
+
+    coin.src =
+      "./10 Centavos.png";
+
+  }
+
+  else if(valor === 0.25){
+
+    coin.src =
+      "./25 Centavos.png";
+
+  }
+
+  else if(valor === 0.50){
+
+    coin.src =
+      "./50 Centavos.png";
+
+  }
+
+  else{
+
+    coin.src =
+      "./1 Real.png";
+
+  }
+
+  coin.className = "coin";
+
+  coin.style.left =
+    Math.random()*180 + "px";
+
+  document.getElementById(
+    "coins"
+  ).appendChild(coin);
+
+  setTimeout(()=>{
+
+    coin.remove();
+
+  },1500);
 
 }
 
